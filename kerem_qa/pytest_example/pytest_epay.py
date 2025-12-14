0909f7a59159b25a3919d3fab501c945711b9433
User <maram15hassoun@gmail.com>
2025-12-14 13:54:31 +0200

    ebay example_2

--- kerem_qa/pytest_example/pytest_epay.py
+++ kerem_qa/pytest_example/pytest_epay.py
@@ -1,8 +1,10 @@
-
+import time
 import unittest
 
+import pytest
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 from kerem_qa.selenium_example.seleniumBaseDalya import seleniumBaseDalya
 
@@ -33,6 +35,43 @@ class pytest_epay(unittest.TestCase):
         url = self.driver.current_url
         assert item in url,"url should contain the word 'shirt'"
         print("item was find successfully")
+
+    def test_Keyword_options(self):
+        item = "shirt"
+        print("test start")
+        advanced_button = self.driver.find_element(By.LINK_TEXT, "Advanced")
+        advanced_button.click()
+
+        enter_button = self.driver.find_element(By.CLASS_NAME,"textbox__control")
+        enter_button.send_keys(item)
+
+        keyword_options = Select(self.driver.find_element(By.ID,"s0-1-20-4[0]-7[1]-_in_kw"))
+        keyword_options.select_by_index(2)
+        print("keyword options was found successfully")
+
+        enter_button.send_keys(Keys.ENTER)
+        url = self.driver.current_url
+        assert item in url, "url should contain the word 'shirt'"
+        print("item was find successfully")
+
+    def test_checkbox(self):
+        advanced_button = self.driver.find_element(By.LINK_TEXT, "Advanced")
+        advanced_button.click()
+
+        check_box = self.driver.find_element(By.ID,"s0-1-20-5[1]-[1]-LH_Complete")
+        self.base.click_and_assert_on_element(check_box)
+
+        radio_button = self.driver.find_element(By.ID,"s0-1-20-6[4]-[0]-LH_ItemCondition")
+        radio_button.click()
+
+        print("radio button was clicked")
+
+
+
+
+
+
+
 
 
 
